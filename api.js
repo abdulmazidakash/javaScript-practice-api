bbe49beea868b8da9a9042b1370a70c9ce021d7b
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-02 16:17:57 +0600

    module 34 api practice

---
+++ api.js
@@ -0,0 +1,36 @@
+// let a = 5;
+
+// function add(num1, num2){
+// 	const result = num1 + num2 + a;
+// 	return result;
+// }
+
+// const sum = add(4,5);
+// console.log(sum);
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+// doSomething()
+
+// setTimeout(doSomething)
+
+// setTimeout(()=>{
+// 	console.log('lazy logged');
+// }, 4000)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+
+// function doSomething(){
+// 	console.log(3);
+// }
+
+console.log('i');
+// console.log('eat');
+
+setTimeout(()=>{
+	console.log('eat');
+}, 2000)
+console.log('ice cream');
